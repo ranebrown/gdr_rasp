d08708782aa9319a3aa275fd6f06891ae939c3fe
rane brown <rane.brown@gmail.com>
2016-02-13 16:52:54 -0700

    basic uart test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,55 @@
 #include <cstdlib>
 #include <iostream>
+extern "C" 
+{
+    #include "uart.h"
+}
 
 using namespace std;
 
 int main(void)
 {
+    int uart0_filestream = initUART();
+    unsigned char tx_buffer[20];
+    unsigned char *p_tx_buffer;
+
+    p_tx_buffer = &tx_buffer[0];
+    *p_tx_buffer++ = 'H';
+    *p_tx_buffer++ = 'e';
+    *p_tx_buffer++ = 'l';
+    *p_tx_buffer++ = 'l';
+    *p_tx_buffer++ = 'o';
+
+    if (uart0_filestream != -1)
+    {
+        int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));      //Filestream, bytes to write, number of bytes to write
+        if (count < 0)
+        {
+            printf("UART TX error\n");
+        }
+    }
+    while(1) {
+    if (uart0_filestream != -1)
+    {
+        // Read up to 255 characters from the port if they are there
+        unsigned char rx_buffer[256];
+        int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);      //Filestream, buffer to store in, number of bytes to read (max)
+        if (rx_length < 0)
+        {
+            //An error occured (will occur if there are no bytes)
+        }
+        else if (rx_length == 0)
+        {
+            //No data waiting
+        }
+        else
+        {
+            //Bytes received
+            rx_buffer[rx_length] = '\0';
+            printf("%i bytes read : %s\n", rx_length, rx_buffer);
+        }
+    }
+    }
+    close(uart0_filestream);
     return 0;
 }
