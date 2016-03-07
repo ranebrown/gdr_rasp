3f0563f7a293e420cdd18ea0644bade52f20ad73
rane brown <rane.brown@gmail.com>
2016-03-07 14:43:25 -0700

    gpio for cv interrupt

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@ extern "C"
 {
     #include "uart.h"
     #include <unistd.h> // used for sleep XXX
+    #include <wiringPi.h> // gpio
 }
 #include "master.hpp"
 #include "shapeDetect.hpp"
@@ -14,13 +15,18 @@ int main(void)
 {
     // variables
     char val = 0;
-    char *txBuff = &val;
-    char rxBuff[255];
-    int txRes = 1, rxRes = 1;
+    /* char *txBuff = &val; */
+    /* char rxBuff[255]; */
+    /* int txRes = 1, rxRes = 1; */
     float dist = 0;
 
     // uart initialization
-    int uart0_filestream = initUART();
+    /* int uart0_filestream = initUART(); */
+
+    // gpio initialization
+    wiringPiSetup();
+    pinMode(0,OUTPUT);
+    digitalWrite(0,LOW);
 
     // open default camera 0
     VideoCapture capture(0);
@@ -49,16 +55,20 @@ int main(void)
 
         // convert distance to char so it can be sent over uart
         val = (char)dist;
-        if(val <= 72)
+        if(val <= 90 && val > 20)
         {
+            digitalWrite(0,HIGH);
+
             // send data over uart
-            txRes = uartSend(uart0_filestream, txBuff, strlen(txBuff));
-            if(txRes != 0)
-            {
-                printf("bad val\n");
-                // TODO error handling
-            }
+            /* txRes = uartSend(uart0_filestream, txBuff, strlen(txBuff)); */
+            /* if(txRes != 0) */
+            /* { */
+            /*     printf("bad val\n"); */
+            /*     // TODO error handling */
+            /* } */
         }
+        digitalWrite(0,LOW);
+        dist = 0;
 
         // if any key is pressed exit
         /* if(waitKey(30) >= 0) break; */
@@ -66,35 +76,35 @@ int main(void)
 
 
     // send data over uart
-    txRes = uartSend(uart0_filestream, txBuff, strlen(txBuff));
-    if(txRes != 0)
-    {
-        cout<<"Error sending uart data\n";
-        return 0;
-    }
+    /* txRes = uartSend(uart0_filestream, txBuff, strlen(txBuff)); */
+    /* if(txRes != 0) */
+    /* { */
+    /*     cout<<"Error sending uart data\n"; */
+    /*     return 0; */
+    /* } */
 
     // delay so data can be echoed back
-    sleep(1);
+    /* sleep(1); */
 
     // read uart data
-    rxRes = uartRead(uart0_filestream, rxBuff, sizeof(rxBuff));
-    if(rxRes != 0)
-    {
-        cout<<"Error reading uart data\n";
-        return 1;
-    }
+    /* rxRes = uartRead(uart0_filestream, rxBuff, sizeof(rxBuff)); */
+    /* if(rxRes != 0) */
+    /* { */
+    /*     cout<<"Error reading uart data\n"; */
+    /*     return 1; */
+    /* } */
 
-    // print data stored in rx buffer
-    int i = 0;
-    while(rxBuff[i] != '\0')
-    {
-        cout<<rxBuff[i];
-        i++;
-    }
-    cout<<"\n";
+    /* // print data stored in rx buffer */
+    /* int i = 0; */
+    /* while(rxBuff[i] != '\0') */
+    /* { */
+    /*     cout<<rxBuff[i]; */
+    /*     i++; */
+    /* } */
+    /* cout<<"\n"; */
 
-    // close uart
-    close(uart0_filestream);
+    /* // close uart */
+    /* close(uart0_filestream); */
 
     return 0;
 }
